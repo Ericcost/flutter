45158ff1b954c7b9234ba8b6f0733deef5b94ba2
Ericcost <eric.costerousse@laposte.net>
2024-02-17 11:13:47 +0100

    Feature: Add a button to navigate to a Description Page of one item

--- lib/widget/card_widget.dart
+++ lib/widget/card_widget.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:learnflutter/class/item_class.dart';
 import 'package:learnflutter/core/constants.dart';
+import 'package:learnflutter/pages/description_page.dart';
 
 class CardWidget extends StatelessWidget {
-  const CardWidget({
-    super.key,
-    required this.item
-  });
+  const CardWidget({super.key, required this.item});
   final ItemClass item;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return DescriptionPage(
+                item: item,
+              );
+            },
+          ),
+        );
       },
       child: Card(
           child: Container(
@@ -24,12 +31,8 @@ class CardWidget extends StatelessWidget {
                   const SizedBox(height: kDouble5),
                   Image.asset(item.imagePath),
                   Text(item.title,
-                      style:
-                          const TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                          )
-                  ),
+                      style: const TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.bold)),
                   Text('This is the ${item.title} description'),
                   const SizedBox(height: kDouble5)
                 ],
