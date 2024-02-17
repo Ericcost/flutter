2bea10966feecb5d2b469adb8188b36e75f05148
Ericcost <eric.costerousse@laposte.net>
2024-02-17 11:15:44 +0100

    lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learnflutter/pages/home_page.dart';
+import 'package:learnflutter/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const HomePage()
+      home: const WidgetTree()
     );
   }
 }
