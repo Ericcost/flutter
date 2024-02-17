ec1f103b8fb25d6f1ab9ecc207587a971a36a50b
Ericcost <eric.costerousse@laposte.net>
2024-02-17 11:11:41 +0100

    Feature: Create a description constant

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,3 +2,6 @@ const double kDouble5 = 5.0;
 const double kDouble10 = 10.0;
 const double kDouble20 = 20.0;
 const double kDouble40 = 40.0;
+
+const String baconDescription =
+    'White, well groomed hair gently hangs over a long, friendly face. Expressive pink eyes, set a-symmetrically within their sockets, watch hungrily over the river they\'ve nearly died for for so long. Smooth skin gorgeously compliments his hair and cheekbones and leaves a compelling memory of his fortunate survival.';
