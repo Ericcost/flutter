85d214423a374decc7957406bfd6a91c8f5bd31d
Ericcost <eric.costerousse@laposte.net>
2024-02-17 11:42:34 +0100

    Feature: Add Circle Avatar and ListTile widgets

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learnflutter/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -6,7 +7,30 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: const Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/yeah.png'),
+            radius: 60,
+          ),
+          SizedBox(height: kDouble20),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Flutter'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('Flutter@gmail.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text('Flutter Website'),
+          ),
+        ],
+      ),
     );
   }
 }
